312833a4b26a5b278c4dd64514d8f83683813dca
Roberto Alejandro Bargiela <robertobargiela@gmail.com>
2023-06-24 19:03:13 -0300

    - Refactor of chronos summary

--- chronos-summary.sh
+++ chronos-summary.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
-input="./timelog.log"
-prev=0
-curr=0
-elapsed=0
-totalhours=0
-totalmins=0
 
-pickedDate=$@
-cat $input | grep "$pickedDate" > tmp.log
-while IFS="%" read -r datetime timestamp task
-do
+function init(){
+    input="./timelog.log"
+    prev=0;curr=0;elapsed=0
+    pickedDate=$@; cat $input | grep "$pickedDate" > tmp.log
+}
+
+function calculateTaskTime(){
     curr=$timestamp
     if [ $prev -lt 0 ]; then
         prev=$curr
     fi
-    let "secs= ($curr - $prev)"
-    let "mins= ($curr - $prev)/60"
+    let "mins= ($curr - $prev)/60";
+    return $mins;
+}
+
+function printResume(){
     prev=$curr
     if [ "$task" == "Inicio" ]; then
-        IFS=" " read -r day rest
-        echo -e "\n==> $day <=="
+        IFS=" " read -r day rest <<< $datetime
+        echo -e "\n==> $(date -d "$day" +"%a") $day <=="
     else
         echo "($mins mins) [$datetime] - $task"
     fi
-done < ./tmp.log
-rm ./tmp.log
+}
+
+function calculateTotalmins(){
+    if [ "$task" == "Inicio" ]; then
+        let "totalmins=0"
+    else
+        let "totalmins=$totalmins+$mins"
+    fi
+    hours=$(bc <<< "scale=2; $totalmins/60")
+}
+
+function printTotals(){
+    echo -e "\n[Total: ($totalmins mins) $hours h]"
+}
+
+init $@
+while IFS="%" read -r datetime timestamp task
+do
+    calculateTaskTime
+    calculateTotalmins
+    printResume
+done < ./tmp.log;rm ./tmp.log
+printTotals
